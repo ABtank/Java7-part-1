52c74ba07c047123ffa746b97af67a7205d5d7db
Abramov Iurii <abtank@outlook.com>
2021-08-24 04:38:54 +0300

    Spring-boot Hello!

---
+++ less-6-spring-boot/src/main/java/ru/abtank/Less6SpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.abtank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Less6SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Less6SpringBootApplication.class, args);
+	}
+
+}
