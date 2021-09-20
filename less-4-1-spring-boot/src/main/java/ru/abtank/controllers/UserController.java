064693e44d132dbcc651aa38af68b18c2fb9ffbc
Abramov Iurii <abtank@outlook.com>
2021-09-21 00:09:27 +0300

    реализуем сервисы через интерфейсы

--- less-4-1-spring-boot/src/main/java/ru/abtank/controllers/UserController.java
+++ less-4-1-spring-boot/src/main/java/ru/abtank/controllers/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import ru.abtank.dto.ExerciseDto;
 import ru.abtank.dto.UserDto;
 import ru.abtank.persist.entities.Role;
 import ru.abtank.persist.entities.User;
--- less-4-1-spring-boot/src/main/java/ru/abtank/servises/UserService.java
+++ less-4-1-spring-boot/src/main/java/ru/abtank/servises/UserService.java
@@ -1,78 +1,23 @@
 package ru.abtank.servises;
 
-
-import lombok.NoArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import ru.abtank.controllers.UserController;
 import ru.abtank.dto.UserDto;
 import ru.abtank.persist.entities.User;
-import ru.abtank.persist.repositories.RoleRepository;
-import ru.abtank.persist.repositories.UserRepository;
-import ru.abtank.persist.repositories.UserSpecification;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-@Service
-@NoArgsConstructor
-public class UserService {
-    private UserRepository userRepository;
-    private RoleRepository roleRepository;
-    private PasswordEncoder passwordEncoder;
-    private final static Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+public interface UserService {
 
-    @Autowired
-    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
-    @Autowired
-    public void setRoleRepository(RoleRepository roleRepository) {
-        this.roleRepository = roleRepository;
-    }
+    Optional<User> findById (Integer id);
+    Optional<User> findByLogin (String login);
+    List<UserDto> findAll();
+    List<User> findAll(Specification spec);
+    Page<UserDto> findAll(Map<String, String> params, PageRequest pageRequest);
+    User save(User user);
+    void deleteById(Integer id);
 
-    @Autowired
-    public void setUserRepository(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    public Page<UserDto> findAll(Map<String, String> params, PageRequest pageRequest) {
-        Specification<User> spec = UserSpecification.trueLiteral();
-        if (params.containsKey("check_login_filter") && !params.get("login_filter").isBlank()) {
-            spec = spec.and(UserSpecification.loginContains(params.get("login_filter")));
-        }
-        if (params.containsKey("check_email_filter") && !params.get("email_filter").isBlank()) {
-            spec = spec.and(UserSpecification.emailContains(params.get("email_filter")));
-        }
-        return userRepository.findAll(spec, pageRequest).map(UserDto::new);
-    }
-
-    public Optional<User> findById (Integer id){
-        return userRepository.findById(id);
-    }
-    public Optional<User> findByLogin (String login){
-        return userRepository.findByLogin(login);
-    }
-
-    public List<UserDto> findAll() {
-        return userRepository.findAll().stream().map(UserDto::new).collect(Collectors.toList());
-    }
-    public List<User> findAll(Specification spec){
-        return userRepository.findAll(spec);
-    }
-
-    public void save(User user){
-        userRepository.save(user);
-    }
-    public void deleteById(Integer id){
-        userRepository.deleteById(id);
-    }
 }
